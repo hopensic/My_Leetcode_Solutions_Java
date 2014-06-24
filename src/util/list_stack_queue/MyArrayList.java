65a7b94b055e879ec8a4e9172e77898e7519b35c
hopensic <hopensic@gmail.com>
2014-06-24 21:27:15 +0800

    MyArrayList

--- src/util/list_stack_queue/MyArrayList.java
+++ src/util/list_stack_queue/MyArrayList.java
@@ -1,6 +1,7 @@
 package util.list_stack_queue;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class MyArrayList<T> implements Iterable<T> {
 	private static final int DEFAULT_CAPACITY = 10;
@@ -10,7 +11,11 @@ public class MyArrayList<T> implements Iterable<T> {
 	private T[] theItems;
 
 	public MyArrayList() {
+		doClear();
+	}
 
+	public void clear() {
+		doClear();
 	}
 
 	private void doClear() {
@@ -22,6 +27,28 @@ public class MyArrayList<T> implements Iterable<T> {
 		return theSize;
 	}
 
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	public void trimToSize() {
+		ensureCapacity(size());
+	}
+
+	public T get(int idx) {
+		if (idx < 0 || idx >= size())
+			throw new ArrayIndexOutOfBoundsException();
+		return theItems[idx];
+	}
+
+	public T set(int idx, T newVal) {
+		if (idx < 0 || idx >= size())
+			throw new ArrayIndexOutOfBoundsException();
+		T old = theItems[idx];
+		theItems[idx] = newVal;
+		return old;
+	}
+
 	public void ensureCapacity(int newCapacity) {
 		if (newCapacity < theSize)
 			return;
@@ -31,8 +58,69 @@ public class MyArrayList<T> implements Iterable<T> {
 			theItems[i] = old[i];
 	}
 
+	public boolean add(T x) {
+		add(size(), x);
+		return true;
+	}
+
+	public void add(int idx, T x) {
+		if (theItems.length == size())
+			ensureCapacity(size() * 2 + 1);
+		for (int i = theSize; i > idx; i--)
+			theItems[i] = theItems[i - 1];
+		theItems[idx] = x;
+		theSize++;
+
+	}
+
+	public T remove(int idx) {
+		if (idx < 0 || idx >= size())
+			throw new ArrayIndexOutOfBoundsException();
+		T removedItem = get(idx);
+		for (int i = idx; i < theSize - 1; i++)
+			theItems[i] = theItems[i + 1];
+		theSize--;
+		return removedItem;
+	}
+
 	public Iterator iterator() {
-		return null;
+		return new ArrayListIterator();
+	}
+
+	private class ArrayListIterator implements Iterator<T> {
+
+		private int current = 0;
+
+		@Override
+		public boolean hasNext() {
+			return current < size();
+		}
+
+		@Override
+		public T next() {
+			if (!hasNext())
+				throw new NoSuchElementException();
+			return theItems[current++];
+		}
+
+		@Override
+		public void remove() {
+			MyArrayList.this.remove(--current);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		Iterator<Integer> iterator = list.iterator();
+		while (iterator.hasNext()) {
+			if (iterator.next() == 2)
+				System.out.println(11);
+		}
+
 	}
 
 }
