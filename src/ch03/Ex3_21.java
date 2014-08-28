2838976df03569114b5214f0b7d2d0f2da68ef26
oliver_zf <hopensic@gmail.com>
2014-08-28 20:43:24 +0800

    Ex3.21

---
+++ src/ch03/Ex3_21.java
@@ -0,0 +1,17 @@
+package ch03;
+
+public class Ex3_21 {
+	public static final char SLASH = '/';
+	public static final char WILDCARD = '*';
+
+	public static void main(String[] args) {
+		String str = "/* */";
+
+	}
+	
+	public void deal(String str) {
+		char[] array = str.toCharArray();
+		
+	}
+
+}
