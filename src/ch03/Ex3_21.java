29f9095960d5d8a9f6b81ad5e28fa2703dfc4875
oliver_zf <hopensic@gmail.com>
2014-09-01 08:56:48 +0800

    Ex3_21

--- src/ch03/Ex3_21.java
+++ src/ch03/Ex3_21.java
@@ -1,17 +1,37 @@
 package ch03;
 
+import util.list_stack_queue.MyStack;
+
 public class Ex3_21 {
 	public static final char SLASH = '/';
 	public static final char WILDCARD = '*';
+	public static final char LEFT_PARENTHESES = '(';
+	public static final char RIGHT_PARENTHESES = ')';
+	public static final char LEFT_BRACKET = '[';
+	public static final char RIGHT_BRACKET = ']';
+	public static final char LEFT_BRACE = '{';
+	public static final char RIGHT_BRACE = '}';
 
 	public static void main(String[] args) {
 		String str = "/* */";
 
 	}
-	
+
 	public void deal(String str) {
 		char[] array = str.toCharArray();
-		
+		int len = array.length;
+		MyStack<Character> stack = new MyStack<Character>();
+		for (int i = 0; i < len; i++) {
+			switch (array[i]) {
+			case SLASH:
+
+				break;
+
+			default:
+				continue;
+			}
+		}
+
 	}
 
 }
