f421ddf27b696d25606a6c177c8c0e045a539921
oliver <hopensic@gmail.com>
2015-11-08 01:06:25 +0800

    NimGame

---
+++ src/leetcode/NimGame.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class NimGame
+{
+    
+    public static void main(String[] args)
+    {
+    
+    }
+    
+    public boolean canWinNim(int n)
+    {
+        if (n % 4 == 0)
+            return false;
+        else
+            return true;
+    }
+    
+}
